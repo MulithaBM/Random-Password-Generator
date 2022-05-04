d6344dd4ba7cbbd4ca806965b6a55ad7e0aab436
Mulitha Moremada <66194959+MulithaBM@users.noreply.github.com>
2022-05-04 16:01:00 +0530

    Add files via upload

---
+++ passwordGenerator.py
@@ -0,0 +1,11 @@
+import random
+
+def generate_password(length = 8) :
+    password = ""
+    for _ in range(length) :
+        password += chr(random.randint(32, 126))
+    return password
+
+length = int(input("Length : ") or 10)
+password = generate_password(length)
+print(password)

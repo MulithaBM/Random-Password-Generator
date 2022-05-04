5c27a109ca48372d99037deb28a43cd7cf9e1d7b
Mulitha Moremada <66194959+MulithaBM@users.noreply.github.com>
2022-05-04 19:17:05 +0530

    Update passwordGenerator.py

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,4 +1,7 @@
 import random
+import bcrypt
+
+salt = bcrypt.gensalt()
 
 def generate_password(length = 8) :
     password = ""
@@ -6,5 +9,20 @@ def generate_password(length = 8) :
         password += chr(random.randint(32, 126))
     return password
 
+def hash_password(password) :
+    # convert string into corresponding array of bytes
+    b_password = password.encode('utf-8')
+    hashed_password = bcrypt.hashpw(b_password, salt)
+    return hashed_password
+
+def verify_password(password, hashed_password) :
+    b_password = password.encode('utf-8')
+    return bcrypt.checkpw(b_password, hashed_password)
+
 length = int(input("Length : ") or 10)
+
 password = generate_password(length)
+hashed_password = hash_password(password)
+
+# verify passwords
+verification = bcrypt.checkpw(password, hashed_password)
